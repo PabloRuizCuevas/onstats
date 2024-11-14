09d260802c00f2f4719433bbd8c9b27ae7350784
Your Name <pablo.r.c@live.com>
2024-11-14 18:15:45 +0100

    Lgen typing

--- onstats/lgen.py
+++ onstats/lgen.py
@@ -1,27 +1,25 @@
 import asyncio
 import operator
 import uuid
-from typing import Any, AsyncIterator, Callable, Generator, Iterator, TypeVar
-
+from typing import Any, AsyncGenerator, AsyncIterator, Callable, Generator, Iterator, TypeVar
+from __future__ import annotations
 import numpy as np
 
-from onstats.stats import (
-    percentual_diff,
-    var as var_s,
-    wsum as wsum_s,
-    normalize as normalize_s,
-    ma as ma_s,
-    delay as delay_s,
-    corr_xy as corr_xy_s,
-    ath as ath_s,
-    ema as ema_s,
-)
+from onstats.stats import ath as ath_s
+from onstats.stats import corr_xy as corr_xy_s
+from onstats.stats import delay as delay_s
+from onstats.stats import ema as ema_s
+from onstats.stats import ma as ma_s
+from onstats.stats import normalize as normalize_s
+from onstats.stats import percentual_diff
+from onstats.stats import var as var_s
+from onstats.stats import wsum as wsum_s
 
-G = TypeVar("G")
-H = TypeVar("H")
+type UnkIterator[T] = AsyncIterator[T] | Iterator[T]
+type LgenOperable = "Lgen | Iterator | float"
 
-
-async def sequential_azip(gen1: AsyncIterator[G], gen2: AsyncIterator[H]) -> AsyncIterator[tuple[G | None, H | None]]:
+async def sequential_azip[G,H](gen1: AsyncIterator[G], 
+                               gen2: AsyncIterator[H]) -> AsyncIterator[tuple[G, H]]:
     """The bad thing is that as gen1, gen2 can share asycronous deps,
     a task group raises runtime errors"""
     while True:
@@ -33,7 +31,7 @@ async def sequential_azip(gen1: AsyncIterator[G], gen2: AsyncIterator[H]) -> Asy
         yield await anext(gen1), await anext(gen2)
 
 
-class Lgen:
+class Lgen[T]:
     """Locked Generators,
     when next(ge) anext(ge) is executed it calls the next value on the generator and locks
     the value for future next calls till unlock is called.
@@ -41,15 +39,15 @@ class Lgen:
     This class is intended for working both for async and sync generators
     """
 
-    registry = {}
+    registry: dict[uuid.UUID, 'Lgen'] = {}
 
     # could specify the lock by nesting giving additional argument
-    def __init__(self, it: AsyncIterator | Iterator):
-        self.iterator = it  # sinc or async
+    def __init__(self, it: UnkIterator[T]):
+        self.iterator: UnkIterator[T] = it  # sinc or async
         self.cache = None
         self.uuid = uuid.uuid4()  # slow
 
-    def __next__(self):
+    def __next__(self) -> T:
         if self.uuid not in Lgen.registry:
             Lgen.registry[self.uuid] = next(self.iterator)
         return Lgen.registry[self.uuid]
@@ -57,7 +55,7 @@ class Lgen:
     def __iter__(self):
         return self
 
-    async def __anext__(self):
+    async def __anext__(self) -> T:
         if self.uuid not in Lgen.registry:
             Lgen.registry[self.uuid] = await anext(self.iterator)
         return Lgen.registry[self.uuid]
@@ -65,7 +63,7 @@ class Lgen:
     def __aiter__(self):
         return self
 
-    def __getitem__(self, key: int) -> 'Lgen':
+    def __getitem__(self, key: int) -> "Lgen":
         return delay(self, key)
 
     @classmethod
@@ -84,7 +82,7 @@ class Lgen:
                 break
 
     @classmethod
-    async def a_consume(cls, iterators: list["Lgen"]):
+    async def a_consume(cls, iterators: list[Lgen]) -> AsyncGenerator[list[Lgen], None, None]:
         """Pass iterators to consume with time passing"""
         while True:
             cls.unlock()
@@ -93,164 +91,144 @@ class Lgen:
             except StopIteration:
                 break
 
-    @classmethod
-    def ga(cls, func: Callable[[Any], Callable[[Any], "Lgen"]]):
-        """Use as decorator for convert iterators into Ge"""
-
-        def wrapper(*args, **kw):
-            gen = Lgen(func(*args, **kw))
-            return gen
-
-        wrapper.__name__ = func.__name__
-        wrapper.__dict__ = func.__dict__
-        wrapper.__doc__ = func.__doc__
-        return wrapper
-
-    def nclass(self, other, op):
+    def nclass(self, other: LgenOperable, op: Callable[[T, T], T]) -> Lgen[T]:
         """Creates Ge class from two Ge classes
 
         for now it supports either all sync or all async, but could support also mixed
         """
-
+        gen = None
         if isinstance(self.iterator, Iterator):
             if isinstance(other, (int, float)):
-                return self.__class__(op(x, other) for x in self)
+                gen = (op(x, other) for x in self)
             else:
-                return self.__class__(op(x, y) for x, y in zip(self, other))
+                gen = (op(x, y) for x, y in zip(self, other))
 
         elif isinstance(self.iterator, AsyncIterator):
             if isinstance(other, (int, float)):
-                return self.__class__(op(x, other) async for x in self)
+                gen = (op(x, other) async for x in self)
             else:
-                return self.__class__(op(x, y) async for x, y in sequential_azip(self, other))
+                gen = (op(x, y) async for x, y in sequential_azip(self, other))
+        return self.__class__(gen)
 
-    def __add__(self, other: Iterator | float):
+    def __add__(self, other: LgenOperable) -> Lgen[T]:
         return self.nclass(other, operator.add)
 
-    def __radd__(self, other: Iterator | float):
+    def __radd__(self, other: LgenOperable) -> Lgen[T]:
         return self.__add__(other)
 
-    def __sub__(self, other: 'Lgen | Iterator | float'):
+    def __sub__(self, other: LgenOperable) -> Lgen[T]:
         return self.nclass(other, operator.sub)
 
-    def __mul__(self, other: Iterator):
+    def __mul__(self, other: LgenOperable) -> Lgen[T]:
         return self.nclass(other, operator.mul)
 
-    def __rmul__(self, other: Iterator):
+    def __rmul__(self, other: LgenOperable) -> Lgen[T]:
         return self.__mul__(other)
 
-    def __truediv__(self, other: Iterator | float):
+    def __truediv__(self, other: LgenOperable) -> Lgen[T]:
         return self.nclass(other, operator.truediv)
 
-    def __floordiv__(self, other: Iterator):
+    def __floordiv__(self, other: LgenOperable) -> Lgen[T]:
         return self.nclass(other, operator.floordiv)
 
-    def __mod__(self, other: Iterator):
+    def __mod__(self, other: LgenOperable) -> Lgen[T]:
         return self.nclass(other, operator.mod)
 
-    def __pow__(self, other: Iterator):
+    def __pow__(self, other: LgenOperable)-> Lgen[T]:
         return self.nclass(other, operator.pow)
 
-    def __and__(self, other: Iterator):
+    def __and__(self, other: LgenOperable)-> Lgen[T]:
         return self.nclass(other, operator.and_)
 
-    def __eq__(self, other: 'Lgen | Iterator | float'):
+    def __eq__(self, other: LgenOperable)-> Lgen[T]:
         return self.nclass(other, operator.eq)
 
-    def __lt__(self, other: 'Lgen | Iterator | float'):
+    def __lt__(self, other: LgenOperable)-> Lgen[T]:
         return self.nclass(other, operator.lt)
 
-    def __le__(self, other: 'Lgen | Iterator | float'):
+    def __le__(self, other: LgenOperable)-> Lgen[T]:
         return self.nclass(other, operator.le)
 
-    def __gt__(self, other: 'Lgen | Iterator | float'):
+    def __gt__(self, other: LgenOperable)-> Lgen[T]:
         return self.nclass(other, operator.gt)
 
-    def __ge__(self, other: 'Lgen | Iterator | float'):
+    def __ge__(self, other: LgenOperable)-> Lgen[T]:
         return self.nclass(other, operator.ge)
 
+    @classmethod
+    def from_s_gen[T](cls, s_gen: Generator[T, Any, None], data_iter: Lgen[T] | UnkIterator[T]) -> Lgen[T]:
+        """Returns a sync or async iterator given a Locked Generator"""
+        iterator = getattr(data_iter, "iterator", data_iter)
+        match iterator:
+            case it if isinstance(it, AsyncIterator):
+                return Lgen(s_gen.send(d) async for d in data_iter)
+            case it if isinstance(it, Iterator):
+                return Lgen(s_gen.send(d) for d in data_iter)
+            case _:
+                raise TypeError("data_iter.iterator must be an Iterator or AsyncIterator")
 
-def bfor(g: Generator, data_iter: Lgen) -> Lgen:
-    """Returns a sync or async iterator given a Locked Generator"""
-    if isinstance(data_iter.iterator, AsyncIterator):
-        return (g.send(d) async for d in data_iter)
-    elif isinstance(data_iter.iterator, Iterator):
-        return (g.send(d) for d in data_iter)
-    raise
 
-@Lgen.ga
 def sma(data_iter: Lgen, window: int) -> Lgen:
     """moving average of n window lenght"""
-    return bfor(ma_s(window), data_iter)
+    return Lgen.from_s_gen(ma_s(window), data_iter)
 
 
-@Lgen.ga
 def vwma(source: Lgen, volume: Lgen, window: int) -> Lgen:
     """moving average of n window lenght"""
     return sma(source * volume, window) / sma(volume, window)
 
 
-@Lgen.ga
-def ema(data_iter: Lgen, alpha: float | None = None, com: float | None = None, halflife: float | None = None) -> Lgen:
+def ema(data_iter: Lgen, com: float | None = None, alpha: float | None = None, halflife: float | None = None) -> Lgen:
     """moving average of n window lenght"""
-    return bfor(ema_s(alpha, com, halflife), data_iter)
+    return Lgen.from_s_gen(ema_s(alpha, com, halflife), data_iter)
 
 
-@Lgen.ga
 def ath(data_iter: Lgen) -> Lgen:
     """All time high"""
-    return bfor(ath_s(), data_iter)
+    return Lgen.from_s_gen(ath_s(), data_iter)
 
 
-@Lgen.ga
 def wsum(data_iter: Lgen) -> Lgen:
     """window sum"""
-    return bfor(wsum_s(), data_iter)
+    return Lgen.from_s_gen(wsum_s(), data_iter)
 
 
-@Lgen.ga
 def var(data_iter: Lgen, window: int, ddof: int = 1) -> Lgen:
     """variance"""
-    return bfor(var_s(window, ddof), data_iter)
+    return Lgen.from_s_gen(var_s(window, ddof), data_iter)
 
 
-@Lgen.ga
 def diff(data_iter: Lgen) -> Lgen:
     """variance"""
     return data_iter - data_iter[1]
 
 
-@Lgen.ga
 def rdiff(data_iter: Lgen) -> Lgen:
     """variance"""
     # a = data_iter   (a - data_iter[1])/a
-    return bfor(percentual_diff(), data_iter)
+    return Lgen.from_s_gen(percentual_diff(), data_iter)
 
 
-@Lgen.ga
-def delay(data_iter: Lgen, periods: int, default: float = 0):
+def delay(data_iter: Lgen, periods: int, default: float = 0) -> Lgen:
     """Delays returning the value of the generator by n periods,
     returns default while teh periods are reached"""
-    return bfor(delay_s(periods, default), data_iter)
+    return Lgen.from_s_gen(delay_s(periods, default), data_iter)
 
 
-@Lgen.ga
-def normalize(data_iter: Lgen, window: int = 0, sample_freq: int = 10):
+def normalize(data_iter: Lgen, window: int = 0, sample_freq: int = 10) -> Lgen:
     """Normalizes de data online substracting the average and dividing by
     the rolling std"""
-    return bfor(normalize_s(window, sample_freq), data_iter)
+    return Lgen.from_s_gen(normalize_s(window, sample_freq), data_iter)
 
 
-@Lgen.ga
-def corr_xy(data_iter_a: Lgen, data_iter_b: Lgen, window: int, ddof: int = 0):
+def corr_xy(data_iter_a: Lgen, data_iter_b: Lgen, window: int, ddof: int = 0) -> Lgen:
     """correlation"""
-    return bfor(corr_xy_s(window, ddof), Lgen(i for i in zip(data_iter_a, data_iter_b)))
+    return Lgen.from_s_gen(corr_xy_s(window, ddof), Lgen(i for i in zip(data_iter_a, data_iter_b)))
 
 
-@Lgen.ga
-def auto_corr(data_iter: Lgen, window: int, ddof: int = 0):
+def auto_corr(data_iter: Lgen, window: int, ddof: int = 0) -> Lgen:
     """auto correlation"""
-    return bfor(corr_xy_s(window, ddof), data_iter)
+    return Lgen.from_s_gen(corr_xy_s(window, ddof), data_iter)
 
 
 def crossover(a: Lgen, b: Lgen) -> Lgen:
